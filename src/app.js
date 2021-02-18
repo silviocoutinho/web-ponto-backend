e75347c5404c713058c967eafed9a17586384242
silviocoutinho <silviocoutinho@ymail.com>
2021-02-17 23:37:33 -0300

    Add consign

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const app = require('express')();
+const consign = require('consign');
+const { ENV } = require('../.env');
 
 app.get('/', (req, res) => {
   return res.status(200).send('Teste');
--- src/server.js
+++ src/server.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 app.listen(3005, () => {
   console.log('Backend ok...!');
-})
+});
