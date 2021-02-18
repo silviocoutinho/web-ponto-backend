94c1906f10bb223276c17f0d610a4280ed4063dc
Silvio Coutinho <silviofc79@gmail.com>
2021-02-18 19:08:59 -0300

    Setting consign to body parser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
 const consign = require('consign');
 const { ENV } = require('../.env');
+
+consign({ cwd: 'src' }).include('../config/middlewares.js').into(app);
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   return res.status(200).send('Teste');
