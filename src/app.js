1a93e1ffb83d7abec8cfcd4fe7a55eb5ad8c6973
silviocoutinho <silviocoutinho@ymail.com>
2021-02-23 22:15:35 -0300

    Add intercept errors in APP

--- src/app.js
+++ src/app.js
@@ -3,16 +3,27 @@ const bodyParser = require('body-parser');
 const consign = require('consign');
 const { ENV } = require('../.env');
 
-consign({ cwd: 'src' })
+consign({ cwd: 'src', verbose: false })
   .include('config/middleware.js')
+  .then('./services')
   .then('./routes')
   .then('./config/router.js')
   .into(app);
 
-app.use(bodyParser.json());
-
 app.get('/', (req, res) => {
   return res.status(200).send('Teste');
+});
+
+app.use((err, req, res, next) => {
+  const { name, message, stack } = err;
+  if (name == 'ValidationError') res.status(400).json({ error: message });
+  else if (name == 'RecursoIndevidoError')
+    res.status(403).json({ error: message });
+  else {
+    console.log('=========================>', message);
+    res.status(500).json({ name, message, stack }); //stack => caminho do erro
+  }
+  next(err);
 });
 
 module.exports = app;
