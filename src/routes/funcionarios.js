1039458ce6232f06426d5e6f147cbc03f8f6f1cd
Silvio Coutinho <silviocoutinho@ymail.com>
2021-02-25 11:27:10 -0300

    add route that return one record

--- src/routes/funcionarios.js
+++ src/routes/funcionarios.js
@@ -10,5 +10,12 @@ module.exports = app => {
       .catch(err => next(err));
   });
 
+  router.get('/:id', (req, res, next) => {
+    app.services.funcionario
+      .findOne({ fun_id: req.params.id })
+      .then(result => res.status(200).json(result))
+      .catch(err => next(err));
+  });
+
   return router;
 };
