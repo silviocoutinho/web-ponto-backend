0df244bc99633224dbb9f6550419ec61e8d4b597
Silvio Coutinho <silviofc79@gmail.com>
2021-03-01 18:57:16 -0300

    Add ativos and inativos route in funcionarios

--- src/routes/funcionarios.js
+++ src/routes/funcionarios.js
@@ -3,6 +3,20 @@ const express = require('express');
 module.exports = app => {
   const router = express.Router();
 
+  router.get('/ativos/', (req, res, next) => {
+    app.services.funcionario
+      .findAll({ fun_ativo: true })
+      .then(result => res.status(200).json(result))
+      .catch(err => next(err));
+  });
+
+  router.get('/inativos/', (req, res, next) => {
+    app.services.funcionario
+      .findAll({ fun_ativo: false })
+      .then(result => res.status(200).json(result))
+      .catch(err => next(err));
+  });
+
   router.get('/', (req, res, next) => {
     app.services.funcionario
       .findAll()
