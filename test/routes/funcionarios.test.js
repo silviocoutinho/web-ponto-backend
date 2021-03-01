670e2defd63390c3cbe1dd34811601f550c88053
Silvio Coutinho <silviofc79@gmail.com>
2021-03-01 18:21:56 -0300

    Add test return one employee by id

--- test/routes/funcionarios.test.js
+++ test/routes/funcionarios.test.js
@@ -4,6 +4,30 @@ const app = require('../../src/app');
 const VERSION_API = '';
 const MAIN_ROUTE = `${VERSION_API}/funcionarios`;
 
+let admID = 0;
+let lastID = 0;
+
+beforeAll(async () => {
+  lastID = await app
+    .db('funcionarios')
+    .select(['fun_id'])
+    .orderBy('fun_id', 'desc')
+    .first();
+
+  let userAdm = {
+    fun_data_cadastro: new Date(),
+    fun_adm: true,
+    fun_nome: 'Admin',
+    fun_usuario: 'admin',
+    fun_senha: 'Test3D3Senh@',
+    fun_matricula: 101,
+    fun_pis: '1234567890',
+    fun_ativo: true,
+  };
+  const user = await app.db('funcionarios').insert([userAdm], '*');
+  admID = user[0].fun_id;
+});
+
 describe('When listing employees ', () => {
   test('Should return all employees', () => {
     return request(app)
@@ -12,9 +36,17 @@ describe('When listing employees ', () => {
         expect(res.status).toBe(200);
       });
   });
-  test.skip('Should return one employee by ID', () => {});
-  test.skip('Should return actives employees', () => {});
-  test.skip('Should return inactives employees', () => {});
+  test('Should return one employee by ID', () => {
+    return request(app)
+      .get(`${MAIN_ROUTE}/${admID}`)
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.body.fun_nome).toBe('Admin');
+        expect(res.body.fun_usuario).toBe('admin');
+      });
+  });
+  test.skip('Should return active employees', () => {});
+  test.skip('Should return inactive employees', () => {});
   test.skip('Should not return employee when ID is invalid', () => {});
 });
 
