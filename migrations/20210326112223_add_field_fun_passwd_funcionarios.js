d1a92ad8f2713a4440dc9e482311e12eb56d51b1
Silvio Coutinho <silviocoutinho@ymail.com>
2021-03-26 11:54:35 -0300

    Add field fun_passwd in funcionarios

---
+++ migrations/20210326112223_add_field_fun_passwd_funcionarios.js
@@ -0,0 +1,16 @@
+exports.up = async function (knex) {
+  await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+  return knex.schema.alterTable('funcionarios', table => {
+    table
+      .string('fun_passwd')
+      .notNull()
+      .unique()
+      .defaultTo(knex.raw('uuid_generate_v4()'));
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.alterTable('funcionarios', table => {
+    table.dropColumn('fun_passwd');
+  });
+};
