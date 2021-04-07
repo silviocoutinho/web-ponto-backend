f597e543d838861120cfb8b05dfd7c17968e47ff
silviocoutinho <silviocoutinho@ymail.com>
2021-04-06 22:38:46 -0300

    Fix code in delete route

--- src/routes/funcionarios.js
+++ src/routes/funcionarios.js
@@ -48,7 +48,7 @@ module.exports = app => {
   router.delete('/:id', (req, res, next) => {
     app.services.funcionario
       .setInactive(req.params.id)
-      .then(result => res.status(201).json(result))
+      .then(result => res.status(204).json(result))
       .catch(err => next(err));
   });
 
