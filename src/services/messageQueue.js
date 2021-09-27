f95dfb94082fe3936f790b03e3dc1b7152dc7511
silviocoutinho <silviocoutinho@ymail.com>
2021-09-27 10:45:02 -0300

    Fix Error when server RabbitMQ is Down

--- src/services/messageQueue.js
+++ src/services/messageQueue.js
@@ -1,4 +1,5 @@
 const amqp = require('amqplib/callback_api');
+const amqp2 = require('amqplib');
 const { RABBITMQ_SERVER } = require('../../.env');
 const CONN_URL = `amqp://${RABBITMQ_SERVER}:5672`;
 console.log(CONN_URL);
@@ -18,7 +19,7 @@ module.exports = app => {
   const sendMessageToQueue = (message, queueName) => {
     amqp.connect(CONN_URL, (errConn, connection) => {
       if (errConn) {
-        throw new Error('Erro de conexao ');
+        throw new Error('Erro de conexao com servidor RabbitMQ');
       }
       connection.createChannel((errCh, channel) => {
         if (errCh) {
@@ -37,5 +38,28 @@ module.exports = app => {
     });
   };
 
-  return { sendMessageToQueue };
+  const sendMessageToQueue2 = async (message, queueName) => {
+    let resultMessage = { status: 200, message: 'Mensagem enviada!' };
+    await amqp2
+      .connect(CONN_URL)
+      .then(connection => {
+        return connection.createChannel();
+      })
+      .then(channel => {
+        console.log('MESSAGE: ', message, queueName);
+        channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+          persistent: true,
+        });
+        console.log('MESSAGE: ', message, queueName);
+      })
+      .catch(err => {
+        resultMessage = {
+          status: 503,
+          message: 'Erro de conexao com servidor RabbitMQ!',
+        };
+      });
+    return resultMessage;
+  };
+
+  return { sendMessageToQueue, sendMessageToQueue2 };
 };
