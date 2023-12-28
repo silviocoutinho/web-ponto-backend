fad29d7f733f3032bebe2061075ffacfef255eef
Silvio Ferreira Coutinho <silviocoutinho@ymail.com>
2023-12-28 09:37:14 -0300

    feat: add year 2024

--- src/routes/exercicioHolerites.js
+++ src/routes/exercicioHolerites.js
@@ -6,24 +6,28 @@ module.exports = app => {
   router.get('/', (request, response, next) => {
     response.status(200).json([
       {
-        label: '2023',
+        label: '2024',
         value: '1',
       },
       {
-        label: '2022',
+        label: '2023',
         value: '2',
       },
       {
-        label: '2021',
+        label: '2022',
         value: '3',
       },
       {
-        label: '2020',
+        label: '2021',
         value: '4',
       },
       {
-        label: '2019',
+        label: '2020',
         value: '5',
+      },
+      {
+        label: '2019',
+        value: '6',
       },
     ]);
   });
